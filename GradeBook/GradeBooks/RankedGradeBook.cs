a69993a6dc962615ae841ebaafd6f1791351e7dd
Hunter Esposito <hesposito@tutanota.com>
2020-07-30 13:15:27 -0400

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using GradeBook.Enums;
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,41 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+            int x = (int)Math.Round(Students.Count * .2, 0);
+
+            List<double> studentGrades = Students
+                .OrderByDescending(x => x.AverageGrade)
+                .Select(x => x.AverageGrade)
+                .ToList();
+
+            switch (averageGrade)
+            {
+                case var g when g > studentGrades[x]:
+                    return 'A';
+
+                case var g when g > studentGrades[x * 2] && g < studentGrades[x]:
+                    return 'B';
+
+                case var g when g > studentGrades[x * 3] && g < studentGrades[x * 2]:
+                    return 'C';
+
+                case var g when g > studentGrades[x * 4] && g < studentGrades[x * 3]:
+                    return 'D';
+
+                default:
+                    break;
+            }
+
+            return 'F';
         }
     }
 }
